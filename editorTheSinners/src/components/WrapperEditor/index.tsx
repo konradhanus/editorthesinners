7f07e8e27a8a8c44b428ca47af558b5a0e117aa1
Beata Kula <beata.kula@intive.com>
2022-09-03 12:09:49 +0200

    test Betty

--- editorTheSinners/src/components/WrapperEditor/index.tsx
+++ editorTheSinners/src/components/WrapperEditor/index.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export default styled.div`
-  display: flex;
-  alignItems: flex-start;
-  alignContent: flex-start;
+    display: flex;
+    align-items: flex-start;
+    align-content: flex-start;
 `;
