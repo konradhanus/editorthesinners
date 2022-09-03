8ab7d85bd2549ce16177d3f72bc9535dc8c35fb2
Beata Kula <beata.kula@intive.com>
2022-09-03 13:34:36 +0200

    adding  storybook

--- editorTheSinners/src/App.tsx
+++ editorTheSinners/src/App.tsx
@@ -1,16 +1,17 @@
 import Drawer from "./components/Drawer";
 import Editor from "./components/Editor";
 import WrapperEditor from "./components/WrapperEditor";
+import { dirt1 } from "./assets/index";
 
 function App() {
-  return (
-    <WrapperEditor >
-      <Drawer>
-        <img src="./" />
-      </Drawer>
-      <Editor>Editor</Editor>
-    </WrapperEditor>
-  );
+    return (
+        <WrapperEditor>
+            <Drawer>
+                <img src={dirt1} alt="dirt1" />
+            </Drawer>
+            <Editor>Editor</Editor>
+        </WrapperEditor>
+    );
 }
 
 export default App;
--- editorTheSinners/src/assets/index.tsx
+++ editorTheSinners/src/assets/index.tsx
@@ -1,57 +1,57 @@
-import dirt1 from './dirt1.png';
-import dirt2 from './dirt2.png';
-import dirt3 from './dirt3.png';
-import dirt4 from './4.png';
-import dirt5 from './5.png';
-import dirt6 from './6.png';
-import dirt7 from './7.png';
-import dirt8 from './8.png';
-import dirt9 from './9.png';
-import dirt10 from '.10.png';
-import dirt11 from './11.png';
-import dirt12 from './12.png';
-import dirt13 from './13.png';
-import dirt14 from './14.png';
-import dirt15 from './15.png';
-import dirt16 from './16.png';
-import dirt17 from './17.png';
-import dirt18 from './18.png';
-import background from './background.png';
-import layer1 from './bg_layer1.png';
-import layer2 from './bg_layer2.png';
-import layer3 from './bg_layer3.png';
-import layer4 from './bg_layer4.png';
-import enemy from './enemy.png';
-import wizzard from './tom.png';
-import water from './water.png';
-import box from './box.png';
+import dirt1 from "./dirt1.png";
+import dirt2 from "./dirt2.png";
+import dirt3 from "./dirt3.png";
+import dirt4 from "./4.png";
+import dirt5 from "./5.png";
+import dirt6 from "./6.png";
+import dirt7 from "./7.png";
+import dirt8 from "./8.png";
+import dirt9 from "./9.png";
+import dirt10 from "./10.png";
+import dirt11 from "./11.png";
+import dirt12 from "./12.png";
+import dirt13 from "./13.png";
+import dirt14 from "./14.png";
+import dirt15 from "./15.png";
+import dirt16 from "./16.png";
+import dirt17 from "./17.png";
+import dirt18 from "./18.png";
+import background from "./background.png";
+import layer1 from "./bg_layer1.png";
+import layer2 from "./bg_layer2.png";
+import layer3 from "./bg_layer3.png";
+import layer4 from "./bg_layer4.png";
+import enemy from "./enemy.png";
+import wizzard from "./tom.png";
+import water from "./water.png";
+import box from "./box.png";
 
-export { 
-    dirt1, 
-    dirt2, 
-    dirt3, 
-    dirt4, 
-    dirt5, 
-    dirt6, 
-    dirt7, 
-    dirt8, 
-    dirt9, 
-    dirt10, 
-    dirt11, 
-    dirt12, 
-    dirt13, 
-    dirt14, 
-    dirt15, 
-    dirt16, 
-    dirt17, 
-    dirt18, 
-    layer1, 
-    layer2, 
-    layer3, 
+export {
+    dirt1,
+    dirt2,
+    dirt3,
+    dirt4,
+    dirt5,
+    dirt6,
+    dirt7,
+    dirt8,
+    dirt9,
+    dirt10,
+    dirt11,
+    dirt12,
+    dirt13,
+    dirt14,
+    dirt15,
+    dirt16,
+    dirt17,
+    dirt18,
+    layer1,
+    layer2,
+    layer3,
     layer4,
     wizzard,
-    enemy, 
-    water, 
+    enemy,
+    water,
     box,
-    background
- };
+    background,
+};
