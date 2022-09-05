63dc081e53b03784901228b1d4f9c7adb4191d0f
Beata Kula <beata.kula@intive.com>
2022-09-05 10:30:37 +0200

    Adding new drower element

--- editorTheSinners/src/App.tsx
+++ editorTheSinners/src/App.tsx
@@ -1,13 +1,30 @@
 import Drawer from "./components/Drawer";
 import Editor from "./components/Editor";
 import WrapperEditor from "./components/WrapperEditor";
-import { dirt1 } from "./assets/index";
+import DrowerElement from "./components/Drawer/DrowerElement";
 
 function App() {
     return (
         <WrapperEditor>
             <Drawer>
-                <img src={dirt1} alt="dirt1" />
+                <DrowerElement name="dirt1" />
+                <DrowerElement name="dirt2" />
+                <DrowerElement name="dirt3" />
+                <DrowerElement name="dirt4" />
+                <DrowerElement name="dirt5" />
+                <DrowerElement name="dirt6" />
+                <DrowerElement name="dirt7" />
+                <DrowerElement name="dirt8" />
+                <DrowerElement name="dirt9" />
+                <DrowerElement name="dirt10" />
+                <DrowerElement name="dirt11" />
+                <DrowerElement name="dirt12" />
+                <DrowerElement name="dirt13" />
+                <DrowerElement name="dirt14" />
+                <DrowerElement name="dirt15" />
+                <DrowerElement name="dirt16" />
+                <DrowerElement name="dirt17" />
+                <DrowerElement name="dirt18" />
             </Drawer>
             <Editor>Editor</Editor>
         </WrapperEditor>
--- editorTheSinners/src/components/Drawer/index.tsx
+++ editorTheSinners/src/components/Drawer/index.tsx
@@ -1,6 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export default styled.div`
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-gap: 16px;
     border-width: 10px;
     width: 30%;
     background-color: green;
