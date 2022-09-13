23f5585764d839c3b29b6af4c5d5327b06caa44a
Paweł Roguski <pawel.roguski>
2022-09-13 14:10:12 +0200

    Board resize mechanizm created

--- editorTheSinners/src/components/Editor/Editor.tsx
+++ editorTheSinners/src/components/Editor/Editor.tsx
@@ -1,29 +1,22 @@
+import { useContext } from 'react';
 import Tile from './Tile';
+import { EditorContext } from '../EditorContext'
 
 interface Props {
     className: string;
 }
 
-const Editor = ({className}: Props) => (
-    <div data-qa='Editor' className={className}>
-        {[
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6],
-        ].map((arr:number[], x:number) => arr.map((y:number)=> (
-            <Tile x={x} y={y} key={`${x}-${y}`}/>
-        )))}
-    </div>
-);
+const Editor = ({className}: Props) => {
+
+    const editorContext = useContext(EditorContext);
+
+    return (
+        <div data-qa='Editor' className={className}>
+            {editorContext?.boardMatrix.matrix.map((arr:number[], x:number) => arr.map((value: number, y:number)=> (
+                <Tile x={x} y={y} key={`tile-${x}-${y}`}/>
+            )))}
+        </div>
+    );
+}
 
 export default Editor;
