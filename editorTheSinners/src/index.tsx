14fcaa9f75a4c7667523af541d34f18c1ecb292f
Beata Kula <beata.kula@intive.com>
2022-09-26 15:36:19 +0200

    fix versions

--- editorTheSinners/src/index.tsx
+++ editorTheSinners/src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import store from './store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Provider store={store} >
-      <App />
-    </Provider>
-  </React.StrictMode>
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    root
 );
 
 // If you want to start measuring performance in your app, pass a function
