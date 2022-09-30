13dba2a6dd49bff402ce9596be8b0d80285c6999
Beata Kula <beata.kula@intive.com>
2022-09-30 10:14:33 +0200

    tmp wrong version

--- editorTheSinners/src/components/Drawer/ElementListWrapper/__test__/ElementListWrapper.test.tsx
+++ editorTheSinners/src/components/Drawer/ElementListWrapper/__test__/ElementListWrapper.test.tsx
@@ -3,11 +3,8 @@ import ElementListWrapper from "../";
 
 describe("ElementListWrapper component", () => {
     const elementListWrapper = shallow(<ElementListWrapper />);
+
     it("should render ElementListWrapper component", () => {
         expect(elementListWrapper.exists()).toEqual(true);
-    });
-
-    it("renders childs ElementListWrapper components", () => {
-        console.log(elementListWrapper);
     });
 });
--- editorTheSinners/src/components/Drawer/__test__/Drower.test.tsx
+++ editorTheSinners/src/components/Drawer/__test__/Drower.test.tsx
@@ -1,6 +1,8 @@
 import { shallow } from "enzyme";
+import React, { useState } from "react";
 import Drawer from "../Drawer";
 import ElementListWrapper from "../ElementListWrapper";
+import { boardElements, BoardElementExtened } from "../../../assets";
 
 describe("Drawer component", () => {
     const className = "drawerClassName";
@@ -15,8 +17,57 @@ describe("Drawer component", () => {
     });
 
     it("renders childs Drawer components", () => {
+        // const useApiMock = (api: any, defaultValue: any) => {
+        //     let state = defaultValue;
+        //     let setState = (updater: any) => {
+        //         if (typeof updater === "function") {
+        //             state = updater(state);
+        //         } else {
+        //             state = updater;
+        //         }
+        //         ref.api = api({ state, setState });
+        //     };
+        //     let ref = {
+        //         api: api({ state, setState }),
+        //     };
+        //     return ref;
+        // };
+
+        // const mockApi = useApiMock(someApi, someState);
+
+        // mockApi.api.updateName("new name");
+        // expect(mockApi.api.name).toEqual("new name");
+
+        const boardElements: BoardElementExtened[] = [
+            { path: "dirt1", name: "dirt1", valueId: 12, isSelected: false },
+        ];
+
+        // const setBoxesStatus = () => {
+        //     return boardElements;
+        // };
+
+        // const setState = jest.fn();
+        // const useStateSpy = jest.spyOn(React, "useState");
+        // useStateSpy.mockImplementation((boardElements2) => [
+        //     boardElements,
+        //     setState,
+        // ]);
+
+        // Cache original functionality
+        const realUseState = React.useState;
+
+        // Stub the initial state
+        const stubInitialState = boardElements;
+
+        // Mock useState before rendering your component
+        jest.spyOn(React, "useState").mockImplementationOnce(() =>
+            realUseState(stubInitialState)
+        );
+
         expect(drawer.containsMatchingElement(<ElementListWrapper />)).toEqual(
             true
         );
+
+        console.log(drawer.find(ElementListWrapper).render());
     });
 });
